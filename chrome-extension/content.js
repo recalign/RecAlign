2177dca95377f8125583b5b3f307328ceaf53c02
Tian Jin <tjingrant@gmail.com>
2023-04-05 23:07:55 -0400

    periodically watch for new feed.

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,12 +1,14 @@
 // Set up a cache dictionary to store tweets that have been seen before
-var cache = new Set();
+var cache = new Map();
 
 function clean() {
     console.log("Extension running...");
     var articles = document.querySelectorAll("article[data-testid='tweet']");
-    for (var i = 0; i < articles.length; i++) {
-        console.log("[DISCOVERY]", articles[i].textContent.replaceAll("\n", ""));
-    }
+    var all_tweets = [];
+
+    // for (var i = 0; i < articles.length; i++) {
+    //     console.log("[DISCOVERY]", articles[i].textContent.replaceAll("\n", ""));
+    // }
     var tweets = [];
     var filtered_articles = [];
     for (var i = 0; i < articles.length; i++) {
@@ -14,14 +16,15 @@ function clean() {
         var txt_div = articles[i].querySelector("div[data-testid='tweetText']");
         if (txt_div == null) {
             console.log("[DISCOVERY] fail to find tweet text for " +  articles[i].textContent.replaceAll("\n", ""));
+            all_tweets.push("");
             continue;
         }
         var tweet = txt_div.textContent.replaceAll("\n", "");
+        all_tweets.push(tweet);
         // If tweet is new, add it to the list of tweets and add article to filtered_articles and add tweet to cache
         if (!cache.has(tweet)) {
             tweets.push(tweet);
             filtered_articles.push(articles[i]);
-            cache.add(tweet);
         }
     }
 
@@ -45,12 +48,23 @@ function clean() {
         var response = JSON.parse(xhr.responseText);
         console.log("[BACKEND]", response);
         for (var i = 0; i < response.length; i++) {
-            if (!response[i]) {
+            // Set the cache to the corresponding response for the tweet
+            cache.set(tweets[i], response[i]);
+        }
+
+        for (var i = 0; i < articles.length; i++) {
+            // If the cache does not contain the tweet, log the error
+            if (!cache.has(all_tweets[i])) {
+                console.log("[ERROR] cache does not contain tweet " + tweets[i]);
+                continue;
+            }
+            var keep = cache.get(all_tweets[i]);
+            if (!keep) {
                 // Find and hide the closest parent div with data-testid="cellInnerDiv"
-                filtered_articles[i].closest("div[data-testid='cellInnerDiv']").style.display = "none";
-                console.log("[Remove] tweet" + filtered_articles[i].textContent.replaceAll("\n", ""));
+                articles[i].closest("div[data-testid='cellInnerDiv']").style.display = "none";
+                console.log("[REMOVE] tweet" + articles[i].textContent.replaceAll("\n", ""));
             } else {
-                console.log("[KEEP] keeping tweet" + filtered_articles[i].textContent.replaceAll("\n", ""));
+                console.log("[KEEP] keeping tweet" + articles[i].textContent.replaceAll("\n", ""));
             }
         }
     }
