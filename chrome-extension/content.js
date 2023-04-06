660773ad69f9c695f604c57f79623772cfe1670a
Tian Jin <tjingrant@gmail.com>
2023-04-05 22:47:40 -0400

    checkpoint.

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,49 +1,67 @@
-// document.body.innerHTML = document.body.innerHTML.replace("Programming Language", "ChatGPT"); // replace content a and content b with required text
-console.log("hey this might be working.")
-
-// // Run after 5 seconds
-// setTimeout(function() {
+// Set up a cache dictionary to store tweets that have been seen before
+var cache = new Set();
 
 function clean() {
+    console.log("Extension running...");
     var articles = document.querySelectorAll("article[data-testid='tweet']");
-    // Make an empty list
-    var tweets = [];
-    console.log(articles);
-    // // Iterate over the divs
     for (var i = 0; i < articles.length; i++) {
-        // Print the text content of the div
-        // Add the content to tweets:
-        tweets.push(articles[i].textContent.replaceAll("\n", ""));
+        console.log("[DISCOVERY]", articles[i].textContent.replaceAll("\n", ""));
+    }
+    var tweets = [];
+    var filtered_articles = [];
+    for (var i = 0; i < articles.length; i++) {
+        // Find the outermost div with data-testid="tweetText" and get the text content
+        var txt_div = articles[i].querySelector("div[data-testid='tweetText']");
+        if (txt_div == null) {
+            console.log("[DISCOVERY] fail to find tweet text for " +  articles[i].textContent.replaceAll("\n", ""));
+            continue;
+        }
+        var tweet = txt_div.textContent.replaceAll("\n", "");
+        // If tweet is new, add it to the list of tweets and add article to filtered_articles and add tweet to cache
+        if (!cache.has(tweet)) {
+            tweets.push(tweet);
+            filtered_articles.push(articles[i]);
+            cache.add(tweet);
+        }
+    }
+
+    // If there are no new tweets, return
+    if (tweets.length == 0) {
+        return;
     }
 
     preference = "I like reading about adademic research on AI.";
-    // Make a json object with the tweets and preference
     var data = {
         "messages": tweets,
         "preference": preference
     };
-    console.log(data);
+
     // Send the data to the server and log the response to console
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://hiubwe6637gmpslsccd4ofi3de0yaeqa.lambda-url.us-east-2.on.aws/");
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.send(JSON.stringify(data));
     xhr.onloadend = function() {
-        console.log(xhr.responseText);
-        // Parse the response
         var response = JSON.parse(xhr.responseText);
-        // loop over response, which is a list of "yes" or "no", and remove the corresponding tweet
-        // if the response is "no"
+        console.log("[BACKEND]", response);
         for (var i = 0; i < response.length; i++) {
             if (!response[i]) {
-                // Remove the closest parent div with data-testid="cellInnerDiv"
-                articles[i].closest("div[data-testid='cellInnerDiv']").remove();
-                console.log("removing tweet" + articles[i].textContent.replaceAll("\n", ""));
+                // Find and hide the closest parent div with data-testid="cellInnerDiv"
+                filtered_articles[i].closest("div[data-testid='cellInnerDiv']").style.display = "none";
+                console.log("[Remove] tweet" + filtered_articles[i].textContent.replaceAll("\n", ""));
             } else {
-                console.log("keeping tweet" + articles[i].textContent.replaceAll("\n", ""));
+                console.log("[KEEP] keeping tweet" + filtered_articles[i].textContent.replaceAll("\n", ""));
             }
         }
     }
 }
-// Run every 5 seconds for a maximum of 5 times
-setTimeout(clean, 5000);
+
+// Run every 5 secondS, but prevent multiple instances from running at the same time
+setInterval(function() {
+    if (this.inProgress) {
+        return;
+    }
+    this.inProgress = true;
+    clean();
+    this.inProgress = false;
+}, 5000);
