72a91cda03cb4bcccda11a7de411f38d7026e00b
Tian Jin <tjingrant@gmail.com>
2023-04-06 22:56:23 -0400

    Show validation error when user does not specify preference or openai_api_key.

--- chrome-extension/popup.js
+++ chrome-extension/popup.js
@@ -16,15 +16,26 @@ window.addEventListener('load', function () {
     // Get the value of preference from storage and set it to the textarea
     chrome.storage.sync.get(["preference"]).then((result) => {
         console.log("Value currently is " + result.preference);
+        if (result.preference == null || result.preference == "") {
+            result.preference = "";
+            document.getElementById('preference').classList.add("is-invalid");
+        }
         document.getElementById('preference').value = result.preference;
     });
 
     // Get the value of openai_api_key from storage and set it to the textarea
     chrome.storage.sync.get(["openai_api_key"]).then((result) => {
         console.log("OpenAI API key currently is " + result.openai_api_key);
-        // Mask the API key with asterisks except for the initial few letters.
-        var masked_key = result.openai_api_key.substring(0, 4) + "**********";
-        document.getElementById('openai-api-key').value = masked_key;
+        if (result.openai_api_key == null || result.openai_api_key == "") {
+            result.openai_api_key = "";
+            document.getElementById('openai-api-key').classList.add("is-invalid");
+            document.getElementById('openai-api-key').value = "";
+        } else {
+            // Mask the API key with asterisks except for the initial few letters.
+            var masked_key = result.openai_api_key.substring(0, 4) + "**********";
+            document.getElementById('openai-api-key').value = masked_key;
+        }
+        
     });
     document.getElementById('save-button').addEventListener('click', saveOptions);
 });
